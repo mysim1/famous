598df3927e07356580195865030e76ba11f73180
Unknown <kalle.lundfall@gmail.com>
2017-09-12 09:44:00 +0200

    Added support for decoratoring a renderable prototype

--- src/utilities/RenderablePrototype.js
+++ src/utilities/RenderablePrototype.js
@@ -6,8 +6,19 @@
  * Used internally as a return from with() statements that data bind the contents
  */
 export class RenderablePrototype {
+  _decoratorFunctions = [];
+
   constructor(type, options){
     this.type = type;
     this.options = options;
   }
+
+  addDirectlyAppliedDecoratorFunction(decoratorFunction){
+    this._decoratorFunctions.push(decoratorFunction);
+  }
+
+  getDirectlyAppliedDecoratorFunctions() {
+    return this._decoratorFunctions;
+  }
+
 }
